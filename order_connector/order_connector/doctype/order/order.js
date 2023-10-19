b777d1a4e37d5add61feff34073e50f98321bbfe
dawoodjee <adam.daveed@gmail.com>
2023-10-19 13:00:21 +0200

    added field updating code, next: balances

--- order_connector/order_connector/doctype/order/order.js
+++ order_connector/order_connector/doctype/order/order.js
@@ -1,8 +1,25 @@
 // Copyright (c) 2023, Bantoo and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Order', {
+frappe.ui.form.on('Order Item', {
 	// refresh: function(frm) {
 
 	// }
+	qty: function(frm) {
+		update_totals(frm);		
+	},
+	price: function(frm) {
+		update_totals(frm);		
+	}
 });
+
+function update_totals(frm){
+	frappe.call({
+		method: "validate",
+		doc: frm.doc,
+		callback: () => {
+			frm.refresh();
+			//refresh_field('items');
+		}
+	})
+}
--- order_connector/order_connector/doctype/order/order.py
+++ order_connector/order_connector/doctype/order/order.py
@@ -1,8 +1,57 @@
 # Copyright (c) 2023, Bantoo and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
+import json
 from frappe.model.document import Document
 
 class Order(Document):
-	pass
+	@frappe.whitelist()
+	def validate(self):
+		""" update items.balance, items.amount, doc.total_qty, doc.total_amount
+		"""
+		self.update_fields()
+		self.validate_items(self.items)
+
+	def update_fields(self):
+		#frappe.errprint(self.total_qty)
+
+		self.total_qty = 0
+		self.total_amount = 0
+		for item in self.items:
+			item.amount = item.qty * item.price
+			self.total_qty = self.total_qty + item.qty
+			self.total_amount = self.total_amount + item.amount
+
+	def validate_items(self, items):
+		""" ensure no negatives in qty or price
+		"""
+		for item in items:
+			if item.qty < 1:
+				frappe.throw(_("Row # {1}: {0} quantity cannot be less than 1").format(
+					frappe.bold(item.item_name), frappe.bold(item.idx)
+				))
+			if item.price < 1:
+				frappe.throw(_("Row # {1}: {0} price cannot be less than 1").format(
+					frappe.bold(item.item_name), item.idx
+				))			
+
+	def update_item_balance(self):
+		"""	Now (priority 1)
+				- Item Stock Balance and Warehouse
+				- Event: Update on Item Code selection		
+				- Lookup Logic
+					- Default Item WH (packhouse_settings.default_warehouse_first)
+					- Lookup WHs (packhouse_settings.lookup_warehouses)
+					- All Others, except Exclusion list (packhouse_settings.excluded_warehouses)
+			Next (priority 2):
+				- When unit changes: convert to new unit if there's a conversion factor in item, otherwise it should be provided in the order item line (add fields similar to EF)
+		"""
+		packhouse_settings = get_packhouse_settings()
+		# packhouse_settings.default_warehouse_first
+		# packhouse_settings.lookup_warehouses
+		# packhouse_settings.excluded_warehouses
+
+def get_packhouse_settings():
+	return frappe.get_cached_doc("Packhouse Settings")
